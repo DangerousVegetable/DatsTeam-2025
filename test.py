f6294986545e6c5e1ec140469bda7c33a4a418c9
DangerousVegetable <mukoseev.l@yandex.ru>
2025-04-05 01:28:30 +0300

    Add visualizer.

--- test.py
+++ test.py
@@ -7,8 +7,15 @@ game = Game(5, 5, 5)
 game.build_word(Word('абоб', 0, np.array([0,0,0]), 0))
 game.build_word(Word('абоб', 1, np.array([0,0,0]), 1))
 game.build_word(Word('абоб', 2, np.array([0,0,0]), 2))
-game.build_word(Word('овощ', 2, np.array([2,0,0]), 2))
-game.build_word(Word('ололо', 2, np.array([0,2,0]), 0))
+game.build_word(Word('овощ', 3, np.array([2,0,0]), 2))
+game.build_word(Word('ололо', 4, np.array([0,0,2]), 0))
+game.build_word(Word(('балобаш'), 5, np.array([0,1,0]), 0))
+
+# game.build_word(Word('abob', 0, np.array([0,0,0]), 0))
+# game.build_word(Word('abob', 1, np.array([0,0,0]), 1))
+# game.build_word(Word('abob', 2, np.array([0,0,0]), 2))
+# game.build_word(Word('ovos', 3, np.array([2,0,0]), 2))
+# game.build_word(Word('ololo', 4, np.array([0,0,2]), 0))
 print(game.board)
     
     
@@ -21,7 +28,5 @@ def write_map_to_file(map_data, file_name):
         print(f"Error writing map to file: {e}")
 
 
-map = {
-    'board': game.board
-}
+map = game.board
 write_map_to_file(map, 'test2.json')
